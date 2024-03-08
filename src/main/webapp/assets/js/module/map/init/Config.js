ff3e1f15f6b60117992ddb5481d0003db7695c2b
qodlsgy <qodlsgy@gmail.com>
2024-03-08 16:42:03 +0900

    운영, 개발 서버 지오서버 url 분리

--- src/main/webapp/assets/js/module/map/init/Config.js
+++ src/main/webapp/assets/js/module/map/init/Config.js
@@ -1,7 +1,7 @@
 var Config={
 
     vworldBaseMapKey:'9081FF2E-DA77-38FE-A44E-6C3F1D1E0EF0',
-    geoserverWmsUrl:'http://geonpf.iptime.org:60011/geoserver/rcic/wms'
+    geoserverWmsUrl:'https://rcic.lx.or.kr/geoserver/rcic/wms'
 
 
 
